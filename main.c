946802b6aece3078f5946a40e29404bddb67020b
Rats <juliocesarmmc@hotmail.com>
2019-11-23 23:13:47 -0300

    main with input path line command

--- main.c
+++ main.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #include "automaton.h"
+#include "input_automata.h"
 
 int main(int argc, char const *argv[]) {
 
-    state_t a1 = {1, true, NULL};
-    state_t a2 = {2, true, NULL};
-    state_t a3 = {3, true, NULL};
+    read_xml(argv[1]);
 
-    transition_t b2 = {&a2, 'B', ' ', ' ',  NULL}; // b
-    transition_t b1 = {&a1, 'A', ' ', ' ',  &b2};  // a*
-    transition_t b3 = {&a3, 'C', ' ', ' ',  &b2};  // b*
-    transition_t b4 = {&a3, 'C', ' ', ' ',  NULL}; // b*
-
-    a1.transition = &b1;
-    a2.transition = &b3;
-    a3.transition = &b4;
-
-
-    printf("%d\n", execute_automata(&a1, argv[1]));
+    // state_t a1 = {1, true, NULL};
+    // state_t a2 = {2, true, NULL};
+    // state_t a3 = {3, true, NULL};
+    //
+    // transition_t b2 = {&a2, 'B', ' ', ' ',  NULL}; // b
+    // transition_t b1 = {&a1, 'A', ' ', ' ',  &b2};  // a*
+    // transition_t b3 = {&a3, 'C', ' ', ' ',  &b2};  // b*
+    // transition_t b4 = {&a3, 'C', ' ', ' ',  NULL}; // b*
+    //
+    // a1.transition = &b1;
+    // a2.transition = &b3;
+    // a3.transition = &b4;
+    //
+    //
+    // printf("%d\n", execute_automata(&a1, argv[1]));
     return 0;
 }
