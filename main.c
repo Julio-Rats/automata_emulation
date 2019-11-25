2c540200d3b25cef5bd6d8a44aad281b378346f5
Rats <juliocesarmmc@hotmail.com>
2019-11-25 02:10:54 -0300

    main para testes, printando estados e suas transições

--- main.c
+++ main.c
@@ -5,21 +5,15 @@
 int main(int argc, char const *argv[]) {
 
     read_xml(argv[1]);
+    unify();
 
-    // state_t a1 = {1, true, NULL};
-    // state_t a2 = {2, true, NULL};
-    // state_t a3 = {3, true, NULL};
-    //
-    // transition_t b2 = {&a2, 'B', ' ', ' ',  NULL}; // b
-    // transition_t b1 = {&a1, 'A', ' ', ' ',  &b2};  // a*
-    // transition_t b3 = {&a3, 'C', ' ', ' ',  &b2};  // b*
-    // transition_t b4 = {&a3, 'C', ' ', ' ',  NULL}; // b*
-    //
-    // a1.transition = &b1;
-    // a2.transition = &b3;
-    // a3.transition = &b4;
-    //
-    //
-    // printf("%d\n", execute_automata(&a1, argv[1]));
+    for (size_t i = 0; i < number_states; i++)
+    {
+        printf("ID: %d\tTrans: ", states[i].id);
+        for (transition_t* aux = states[i].transition; aux; aux=aux->next_transition)
+            printf("[%d {%s}]\t", aux->state_destiny->id, aux->simbol_transition);
+        printf("\n");
+    }
+    printf("\n");
     return 0;
 }
