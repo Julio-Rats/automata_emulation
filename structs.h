f0110db84252ae9d9a153af31b9ee81297d7d288
Rats <juliocesarmmc@hotmail.com>
2019-11-25 02:10:29 -0300

    novas estruturas para entrada do xml

--- structs.h
+++ structs.h
@@ -13,22 +13,37 @@
 
 typedef int8_t bool;
 typedef struct transition_t transition_t;
+typedef struct stack_move   stack_move;
 typedef struct state_t      state_t;
 
 
-state_t*         state_current_automaton;
+extern state_t*  state_current_automaton;
+extern state_t*  start_state;
 // char             stack_automaton[256] = "";
 // extern bool      stack_enable         = false;
 // extern u_int16_t stack_control        = 0;
 
+extern state_t*  states;
+extern u_int16_t number_states;
+
+extern transition_t*  transitions;
+extern u_int16_t      number_transitions;
 
 struct transition_t
 {
+    state_t*       state_origin;
+    u_int16_t      id_origin;
     state_t*       state_destiny;
-    char           simbol_transition;
+    u_int16_t      id_destiny;
+    char           simbol_transition[64];
+    stack_move*    simbol_stacking;
+    transition_t*  next_transition;
+};
+
+struct stack_move
+{
     char           simbol_read_stack;
     char           simbol_write_stack;
-    transition_t*  next_transition;
 };
 
 struct state_t
